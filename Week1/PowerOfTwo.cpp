36a7ee2d8e39b502543f47bfc28ba7ffc66da247
Das <daskurapati@gmail.com>
2018-07-15 22:43:05 +0530

    power of two

---
+++ Week1/PowerOfTwo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	int iInput = 0;
+	std::cout << "Please enter a number : ";
+	std::cin >> iInput;
+	
+	bool bIsPowerOfTwo = true;
+
+	do 
+	{
+		if (iInput % 2 != 0) {
+			bIsPowerOfTwo = false;
+			break;
+		}
+		iInput /= 2;
+	} while (iInput != 1);//mistake 
+	if (bIsPowerOfTwo)
+		std::cout << "Entered number is power of two";
+	else
+		std::cout << "Entered number is not power of two";
+
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
